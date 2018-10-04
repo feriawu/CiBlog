06cd93115044afe1e8be69f02392b3cb34eae5ff
kronos <feritkj12@gmail.com>
2018-10-04 23:09:13 +0700

    ciBlog v1.0

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,79 +1,183 @@
 <html>
+
 	<head>
+
 		<title>ciBlog</title>
+
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
+
     <link rel="stylesheet" href="https://bootswatch.com/4/superhero/bootstrap.min.css">
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
     <script src="<?php echo base_url(); ?>assets/js/ckeditor/ckeditor.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	</head>
+
 	<body>
 
+
+
     <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+
        <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+
         <span class="navbar-toggler-icon"></span>
+
       </button>
+
       <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
         <ul class="navbar-nav">
-          <li class="nav-item active">
+
+          <li class="nav-item <?php echo show_current_class('home'); ?>">
+
             <a class="nav-link" href="<?php echo base_url(); ?>">Home <span class="sr-only">(current)</span></a>
+
           </li>
-          <li class="nav-item">
+
+          <li class="nav-item <?php echo show_current_class('about'); ?>">
+
             <a class="nav-link" href="<?php echo base_url(); ?>about">About</a>
+
           </li>
-          <li class="nav-item">
+
+          <li class="nav-item <?php echo show_current_class('privacy-policy'); ?>">
+
             <a class="nav-link" href="<?php echo base_url(); ?>privacy-policy">Privacy Policy</a>
+
           </li>
-          <li class="nav-item">
+
+          <li class="nav-item <?php echo show_current_class('posts'); ?>">
+
             <a class="nav-link" href="<?php echo base_url(); ?>posts">Blog</a>
+
           </li>
-          <li class="nav-item">
+
+          <li class="nav-item <?php echo show_current_class('categories'); ?>">
+
             <a class="nav-link" href="<?php echo base_url(); ?>categories">Categories</a>
+
           </li>
-        </ul>
-      </div>
 
 
-        <ul class="navbar-nav float-right">
           <?php if($this->session->userdata('logged_in')) : ?>
+
             <li class="nav-item dropdown">
+
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
               <img class="sm-img" src="<?php echo base_url('assets/images/posts/noimage.png'); ?>">
+
                 <?php echo $this->session->userdata('username'); ?>
+
               </a>
+
               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+
                 <a class="dropdown-item" href="<?php echo base_url(); ?>posts/create">Crete Post</a>
+
                 <a class="dropdown-item" href="<?php echo base_url(); ?>categories/create">Create Categories</a>
+
                 <a class="dropdown-item" href="<?php echo base_url(); ?>users/logout">Logout</a>
+
               </div>
+
             </li>
+
         <?php endif; ?>
+
           
 
+
+
         <?php if(!$this->session->userdata('logged_in')) : ?>
+
             <li class="nav-item">
+
               <a class="nav-link" href="<?php echo base_url(); ?>users/login">Login</a>
+
             </li>
-            <li class="nav-item slash">
-              |
-           </li>
+
             <li class="nav-item">
+
               <a class="nav-link" href="<?php echo base_url(); ?>users/register">Register</a>
+
             </li>    
+
         <?php endif; ?>
+
       </ul>
+
     </nav>       
 
+
+
     <div class="container fix-margin">
+
+      
+
       <!-- Flash messages -->
 
       <?php foreach ($this->session->flashdata() as $flashdata) : ?>
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-          <?php echo $flashdata; ?>
-          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
+
+       
+
+        <?php if ($this->session->flashdata('login_failed')): ?>
+
+          <div class="alert alert-warning alert-dismissible fade show" role="alert">
+
+            <?php echo $flashdata; ?>
+
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+
+              <span aria-hidden="true">&times;</span>
+
+            </button>
+
+          </div>
+
+
+
+        <?php elseif ($this->session->flashdata('post_deleted')): ?>
+
+          <div class="alert alert-danger alert-dismissible fade show" role="alert">
+
+              <?php echo $flashdata; ?>
+
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+
+              <span aria-hidden="true">&times;</span>
+
+            </button>
+
+          </div>
+
+
+
+        <?php else: ?>
+
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+
+              <?php echo $flashdata; ?>
+
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+
+              <span aria-hidden="true">&times;</span>
+
+            </button>
+
+          </div>
+
+        <?php endif; ?>
+
+
+
       <?php endforeach; ?>
